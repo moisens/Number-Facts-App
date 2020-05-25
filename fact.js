9d80aea50d5c087152f9b08da4de3c6b42ab6784
moise <mose.ns@hotmail.com>
2020-05-25 14:42:23 +0200

    Modification, separate added an output for the fetch fact

--- fact.js
+++ fact.js
@@ -1,37 +1,43 @@
+//AJAX Variables
 let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 let numberInput = document.querySelector('#numberInput');
 
-//numberInput.addEventListener('input', getFactAjax);
-numberInput.addEventListener('input', getFactFetchs);
+//Fetch variables
+let factFetch = document.querySelector('#factFetch');
+let factTextFetch = document.querySelector('#factTextFetch');
+let numberInputFetch = document.querySelector('#numberInputFetch'); 
+
+numberInput.addEventListener('input', getFactAjax);
+numberInputFetch.addEventListener('input', getFactFetchs);
 
 //Get the value of the input With Ajax
-//function getFactAjax(){
-//   let number = numberInput.value;
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'http://numbersapi.com/'+number, true);
-//
-//   xhr.onload = function(){
-//      if(this.status == 200 && number !=''){
-//         fact.style.display = 'block';
-//         factText.innerText = this.responseText;
-//      }
-//   }
-//
-//   xhr.send();
-//
-//}
+function getFactAjax(){
+   let number = numberInput.value;
+   let xhr = new XMLHttpRequest();
+   xhr.open('GET', 'http://numbersapi.com/'+number, true);
+
+   xhr.onload = function(){
+      if(this.status == 200 && number !=''){
+         fact.style.display = 'block';
+         factText.innerText = this.responseText;
+      }
+   }
+
+   xhr.send();
+
+}
 
 //Get the value of the input with Fetch()
 
 function getFactFetchs(){
-   let number = numberInput.value;
+   let number = numberInputFetch.value;
    fetch('http://numbersapi.com/'+number)
       .then((res) => res.text())
       .then((data) => {
          if(number != ''){
-            fact.style.display = 'block';
-            factText.innerText = data;
+            factFetch.style.display = 'block';
+            factTextFetch.innerText = data;
          }
       })
       .catch((error) => console.log(error));
